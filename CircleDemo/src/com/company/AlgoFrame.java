9e3106d9863034d3c694b1705169493901b0862e
MappleTT <1653516704@qq.com>
2019-08-12 15:26:02 +0800

    这是一次添加操作

---
+++ CircleDemo/src/com/company/AlgoFrame.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+public class AlgoFrame extends JFrame {
+
+    private int canvasHeight;
+    private int canvasWidth;
+
+    public AlgoFrame(String title,int canvasWidth,int canvasHeight){
+
+        super(title);
+        this.canvasWidth=canvasWidth;
+        this.canvasHeight=canvasHeight;
+        AlgoCanvas canvas=new AlgoCanvas();
+        canvas.setPreferredSize(new Dimension(canvasWidth,canvasHeight));
+        setContentPane(canvas);
+
+        pack();
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public AlgoFrame(String title){
+           this(title,1024,768);
+    }
+
+    public int getCanvasHeight() {
+        return canvasHeight;
+    }
+    public int getCanvasWidth() {
+        return canvasWidth;
+    }
+
+    //设置自己的数据
+    private Circle [] circles;
+    public void render(Circle []circles){
+        this.circles=circles;
+        repaint();
+    }
+
+
+    private class AlgoCanvas extends JPanel{
+
+        public AlgoCanvas(){
+            super(true);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d=(Graphics2D) g;
+            //抗锯齿
+            RenderingHints hints=new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                                                    RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.addRenderingHints(hints);
+
+            //具体绘制
+            AlgoVisHelper.setStrokeWidth(g2d,1);
+            AlgoVisHelper.setColor(g2d);
+            for(Circle circle:circles){
+                if(!circle.isFilled)
+                    AlgoVisHelper.strokeCircle(g2d,circle.x,circle.y,circle.getR());
+                else
+                    AlgoVisHelper.fillCircle(g2d,circle.x,circle.y,circle.getR());
+            }
+        }
+
+        @Override
+        public Dimension getPreferredSize(){
+            return new Dimension(canvasWidth,canvasHeight);
+        }
+
+
+    }
+}
---
+++ CircleDemo/src/com/company/AlgoVisualizer.java
@@ -0,0 +1,83 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class AlgoVisualizer {
+
+    private Circle []circles;
+    private AlgoFrame frame;
+    private boolean isAnimated=true;
+
+    public AlgoVisualizer(int sceneWidth,int sceneHeight,int N){
+        circles =new Circle[N];
+        int R=50;
+        for(int i=0;i<N;i++){
+            int x=(int)(Math.random()*(sceneWidth-2*R))+R;
+            int y=(int)(Math.random()*(sceneHeight-2*R))+R;
+            int vx=(int)(Math.random()*11)-7;
+            int vy=(int)(Math.random()*11)-7;
+            circles[i]=new Circle(x,y,R,vx,vy);
+        }
+
+        //事件分发线程,初始化视图
+        EventQueue.invokeLater(()->{
+            frame=new AlgoFrame("Welcome",sceneWidth,sceneHeight);
+            frame.addKeyListener(new AlgoKeyListener());
+            frame.addMouseListener(new AlgoMouseListener());
+            new Thread(()->{
+                    run();
+            }).start();
+
+        });
+
+    }
+
+    //动画逻辑
+    public void run(){
+        while(true){
+            //绘制数据
+            frame.render(circles);
+            AlgoVisHelper.pause(7);
+
+            //更新数据
+            if(isAnimated)
+                for(Circle circle:circles)
+                    circle.move(0,0,frame.getCanvasWidth(),frame.getCanvasHeight());
+        }
+    }
+
+    public class AlgoKeyListener extends KeyAdapter{
+        @Override
+        public void keyReleased(KeyEvent e) {
+            if(e.getKeyChar()==' '){
+                    isAnimated=!isAnimated;
+            }
+        }
+    }
+
+    private class AlgoMouseListener extends MouseAdapter{
+        @Override
+        public void mousePressed(MouseEvent e) {
+            e.translatePoint(0,
+                    -(frame.getBounds().height-frame.getCanvasHeight()));
+            //System.out.println(e.getPoint());
+
+            for(Circle circle:circles){
+                if(circle.contain(e.getPoint())){
+                        circle.isFilled=!circle.isFilled;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int sceneWidth=800;
+        int sceneHeight=800;
+        int N=15;
+        AlgoVisualizer algoVisualizer=new AlgoVisualizer(sceneWidth,sceneHeight,N);
+    }
+
+}
